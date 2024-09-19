8b915eadbb0caa0a4df13561da10bf55efd1e462
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-09-19 12:13:03 +0800

    +hello.c, +guess.c

---
+++ hello.c
@@ -0,0 +1,32 @@
+//
+// Created by hfwei on 2024/9/19.
+//
+
+// single-line comment
+// Created by hfwei on 2023/9/15.
+//
+
+/*
+ * this is a multi-line comment
+ * this is a multi-line comment
+ */
+
+// directive
+// .h: header file
+// stdio: standard input/output
+#include <stdio.h>
+
+/*
+ * main function
+ * y <- f(x)
+ * int: integer (return)
+ */
+int main(void) {
+  // "hello world\n": string
+  // printf: print + f (format)
+  printf("Hello World\n");
+
+  // return statement (return to operating system)
+  // 0: exit code
+  return 0;
+}
