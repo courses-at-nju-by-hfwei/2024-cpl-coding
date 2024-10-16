ce13b9ea92081375d3593ce92b03102b9398232c
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-10-16 17:36:29 +0800

    +4-loops/

---
+++ 4-loops/insertion-sort-bsearch.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_LEN 10000
+#define RANGE 10
+
+int main() {
+  int numbers[MAX_LEN] = { 0 };
+
+  int size = 0;
+  scanf("%d", &size);
+
+  srand(time(NULL));
+  for (int i = 0; i < size; i++) {
+    numbers[i] = rand() % RANGE;
+  }
+
+  // print the original array
+  for (int i = 0; i < size; i++) {
+    printf("%d ", numbers[i]);
+  }
+  printf("\n");
+
+  // insertion sort with binary search
+  for (int i = 1; i < size; i++) {
+    int key = numbers[i];
+    int low = 0;
+    int high = i - 1;
+
+    int pos = -1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (key > numbers[mid]) {
+        low = mid + 1;
+      } else if (key < numbers[mid]) {
+        high = mid - 1;
+      } else {
+        pos = mid;
+        low = mid + 1;
+      }
+    }
+
+    if (pos == -1) {
+      pos = low;
+    } else {
+      pos++;
+    }
+
+    for (int j = i - 1; j >= pos; j--) {
+      numbers[j + 1] = numbers[j];
+    }
+
+    numbers[pos] = key;
+
+    for (int i = 0; i < size; i++) {
+      printf("%d ", numbers[i]);
+    }
+    printf("\n");
+  }
+
+  // print the sorted array
+  for (int i = 0; i < size; i++) {
+    printf("%d ", numbers[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
