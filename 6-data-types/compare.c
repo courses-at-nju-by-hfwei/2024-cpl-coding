ad2a61cd2835f38340677602c2d3dce4979394f8
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-10-31 16:09:56 +0800

    compare.c

--- 6-data-types/compare.c
+++ 6-data-types/compare.c
@@ -1,15 +1,14 @@
 /**
- * file: compare.c
+ * See
+ * https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
  *
- * See https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
- *
- * Created by hfwei on 2022/11/10.
+ * Created by hfwei on 2024/10/31.
  */
 
 #include <float.h>
 #include <math.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define EPSILON 1e-5
 
@@ -17,12 +16,12 @@ bool IsEqual(double x, double y);
 
 int main() {
   printf("%d\n", IsEqual(DBL_MAX, DBL_MAX - 100));
+  printf("%d\n", !islessgreater(DBL_MAX, DBL_MAX - 100));
 
   printf("%.50f\n", DBL_MAX - (DBL_MAX - 100));
+  printf("%d\n", !islessgreater(DBL_MAX, DBL_MAX - 100));
 
   return 0;
 }
 
-bool IsEqual(double x, double y) {
-  return fabs(x - y) <= EPSILON;
-}
+bool IsEqual(double x, double y) { return fabs(x - y) <= EPSILON; }
