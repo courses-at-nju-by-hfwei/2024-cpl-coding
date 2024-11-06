d63a6982942246243237e1b7108d85a2d1574af3
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-11-06 17:30:31 +0800

    template/: +7-pointers-arrays/

---
+++ template/7-pointers-arrays/pointer-array.c
@@ -0,0 +1,11 @@
+// Created by hfwei on 2024/11/6.
+
+int main(void) {
+  /********** On array name **********/
+  /********** On array name **********/
+
+  /********** On malloc/free **********/
+  /********** On malloc/free **********/
+
+  return 0;
+}
