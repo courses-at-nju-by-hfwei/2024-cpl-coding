492ec02ea210c0a79ccc8be9d7f3815cf5d8af9b
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-11-27 19:14:37 +0800

    +9-double-pointers/

---
+++ 9-double-pointers/scores.c
@@ -0,0 +1,78 @@
+// Created by hengxin on 11/27/2024.
+
+// Python Tutor Visualization:
+// (1) https://tinyurl.com/scores-of-musicians
+// (2) https://tinyurl.com/scores-of-musicians-malloc
+// https://tinyurl.com/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_OF_MUSICIANS 4
+#define NUM_OF_SCORES 3
+
+void Print(const int table[][NUM_OF_SCORES], int num_of_musicians);
+
+int main() {
+  // C, Java, Python scores of several musicians
+
+  // TODO: (1) initialize scores with a 2D array
+  // const int scores[NUM_OF_MUSICIANS][NUM_OF_SCORES] = {
+  //     { 0, 10, 20 },
+  //     { 10, 20, 30 },
+  //     { 20, 30, 40 },
+  //     { 30, 40, 50 },
+  // };
+
+  // TODO: (2) Dynamically allocate memory for scores
+  // malloc here
+  int (*scores)[NUM_OF_SCORES] = malloc(NUM_OF_MUSICIANS * sizeof(*scores));
+  if (scores == NULL) {
+    return 0;
+  }
+
+  // fill in data here
+  for (int i = 0; i < NUM_OF_MUSICIANS; ++i) {
+    for (int j = 0; j < NUM_OF_SCORES; ++j) {
+      scores[i][j] = i * j;
+    }
+  }
+
+  // print it here
+  Print(scores, NUM_OF_MUSICIANS);
+
+  // ptr_scores here
+  int (*ptr_scores)[NUM_OF_SCORES] = scores;
+  printf("ptr_scores[3][2] = %d\n",
+         (*(ptr_scores + 3))[2]);
+
+  // do not forget to free it
+  free(scores);
+
+  return 0;
+}
+
+// table: int table[][COL]
+// int table[]: int *table
+// int table[][COL]: int (*table)[COL]
+void Print(const int table[][NUM_OF_SCORES], int num_of_musicians) {
+  for (int i = 0; i < num_of_musicians; i++) {
+    for (int j = 0; j < NUM_OF_SCORES; j++) {
+      // table[i][j]: *(*(table + i) + j)
+      // table: int (*)[COL]
+      // table + i: int (*)[COL]
+      // *(table + i): int *
+      // *(table + i) + j: int *
+      // *(*(table + i) + j): int
+      printf("table[%d][%d]: %d\n\n",
+             i, j, table[i][j]);
+
+      printf("table: %p\n", table);
+      printf("table + %d: %p\n", i, table + i);
+      printf("*(table + %d): %p\n", i, *(table + i));
+      printf("*(table + %d) + %d: %p\n", i, j, *(table + i) + j);
+      printf("*(*(table + %d) + %d): %d\n", i, j, *(*(table + i) + j));
+    }
+    printf("\n\n");
+  }
+}
---
+++ 9-double-pointers/selection-sort-strings.c
@@ -0,0 +1,74 @@
+// Created by hfwei on 2024/11/27.
+
+// Python Tutor Visualization: https://tinyurl.com/array-of-musicians (LEN 3)
+// https://tinyurl.com/
+
+#include <stdio.h>
+#include <string.h>
+
+#define LEN 10
+
+void SelectionSort(const char *arr[], int len);
+int GetMinIndex(const char *arr[], int begin, int end);
+void Swap(const char **left, const char **right);
+void Print(const char *arr[], int len);
+
+int main(void) {
+  const char *musicians[LEN] = {
+      "Luo Dayou",
+      "Cui Jian",
+      "Dou Wei",
+      "Zhang Chu",
+      "Wan Qing",
+      "Li Zhi",
+      "Yao",
+      "ZuoXiao",
+      "ErShou Rose",
+      "Hu Mage",
+  };
+
+  Print(musicians, LEN);
+  SelectionSort(musicians, LEN);
+  Print(musicians, LEN);
+
+  return 0;
+}
+
+void SelectionSort(const char *arr[], int len) {
+  for (int i = 0; i < len; i++) {
+    int min_index = GetMinIndex(arr, i, len);
+
+    // arr: char *[]
+    // char**
+    // const char **
+    Swap(arr + i, arr + min_index);
+  }
+}
+
+int GetMinIndex(const char *arr[], int begin, int end) {
+  const char *min = arr[begin];
+  int min_index = begin;
+
+  for (int i = begin + 1; i < end; ++i) {
+    if (strcmp(arr[i], min) < 0) {
+      min = arr[i];
+      min_index = i;
+    }
+  }
+
+  return min_index;
+}
+
+void Swap(const char **left, const char **right) {
+  const char *temp = *left;
+  *left = *right;
+  *right = temp;
+}
+
+void Print(const char *arr[], int len) {
+  for (int i = 0; i < len; i++) {
+    // arr[i]
+    printf("%s\n", arr[i]);
+  }
+  printf("\n");
+}
